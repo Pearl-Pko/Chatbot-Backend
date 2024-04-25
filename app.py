01cfa310aa7f2dc0beaf3062752cc68d0207f77e
Pearl-Pko <osamuede75@gmail.com>
2024-04-25 09:22:32 +0100

    Added support for NER; entities are extracted from the question and a response is tailored to the question

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from joblib import load 
 import json 
 import random
+from converse import ResponseGeneratorFactory
 
 #loads the necessary data 
 pipeline = load("model.joblib")
 with open("data.json", "r") as file:
-    intents = json.loads(file.read())   
+    intents = json.loads(file.read()) 
+    
+with open("course info.json", "r") as file:
+    course_info = json.loads(file.read())   
+
+["Prerequisites", "Credit Load", "Course Name", "Course Status", "Level Courses", "Semester Courses", "HOD Inquiry", "Information about Lecturer"]
 
 def main():
     intent_ids = list(zip(intents.keys(), intents.values()))
@@ -17,9 +23,11 @@ def main():
         intent = intent_ids[intent_id]
         intent_tag = intent[0]
         print(intent_tag)
-        intent_res = intent[1]
+        responseGenerator = ResponseGeneratorFactory.getResponseGenerator(intent_tag)
+        res = responseGenerator.response(intents, course_info, intent_tag, user_input)
+        # intent_res = intent[1]
         
-        res = random.choice(intent_res)
+        res = random.choice(res)
         print(f"AI: {res}")
 
 main()
